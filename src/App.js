c98e456affca821f57709c87ac7e04fd1d76f2a2
Sravanth-007 <79243593+Sravanth-007@users.noreply.github.com>
2021-10-21 08:13:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React , {Component} from 'react' ;
+import Navbar from './components/navbar';
+import  Header from './components/header';
+import  Homeslider from './components/homeslider';
+import Patners from './components/patners';
+import  Latestevents from './components/latestevents';
+import  Popularcourses from './components/popularcourses';
+import  Testimonial from './components/testimonial';
+import  Grid from './components/grid';
+import  Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/style.css';
+import uuid from 'uuid';
+
+
+
+class App extends Component{
+
+    render(){
+
+
+        return(
+        <div>
+        <Navbar />
+        <Header />
+        <Homeslider />
+        <Patners />
+        <Latestevents />
+        <Popularcourses />
+        <Testimonial />
+        <Grid />
+        <Footer />
+          
+           
+             </div>
+       
+               
+        );
+    }
+
+}
+
+
+export default App;
